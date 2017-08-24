996f7056ccd08861aee949f25f44b97558bd32ea
Brittany Kerr and Nicole Freed <brittany@>
2017-08-24 14:19:30 -0700

    update model with default funding amount of 0

--- src/app/project.model.ts
+++ src/app/project.model.ts
@@ -1,3 +1,4 @@
 export class Project {
-  constructor(public title: string, public owner: string, public description: string, public mission: string, public goal: number, public funding: number, public rewards: string[]) { }
+  funding: number = 0;
+  constructor(public title: string, public owner: string, public description: string, public mission: string, public goal: number, public rewards: string[]) { }
 }
