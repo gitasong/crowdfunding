842ed3d10a495c90e89e18640a091fe56458afa0
Brittany Kerr and Nicole Freed <nicole@>
2017-08-24 09:01:55 -0700

    import ProjectService; add to providers and constructor

--- src/app/edit-project/edit-project.component.ts
+++ src/app/edit-project/edit-project.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Project } from '../project.model';
+import { ProjectService } from '../project.service';
 
 @Component({
   selector: 'app-edit-project',
   templateUrl: './edit-project.component.html',
-  styleUrls: ['./edit-project.component.css']
+  styleUrls: ['./edit-project.component.css'],
+  providers: [ProjectService]
 })
 export class EditProjectComponent implements OnInit {
   @Input() selectedProject;
 
-  constructor() { }
+  constructor(private projectService: ProjectService) { }
 
   ngOnInit() {
   }
