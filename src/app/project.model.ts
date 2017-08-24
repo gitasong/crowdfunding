903c1780ab0276256a71fe3cd4da3c8cb68a800e
Brittany Kerr and Nicole Freed <brittany@>
2017-08-24 09:34:38 -0700

    add mission and rewards to model attributes

--- src/app/project.model.ts
+++ src/app/project.model.ts
@@ -1,3 +1,3 @@
 export class Project {
-  constructor(public title: string, public owner: string, public description: string, public goal: number) { }
+  constructor(public title: string, public owner: string, public description: string, public mission: string, public goal: number, public rewards: string[]) { }
 }
