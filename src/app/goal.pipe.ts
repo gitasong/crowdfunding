30536030265d593b7584a1883bca742c11240c39
Brittany Kerr and Nicole Freed <nicole@>
2017-08-24 14:29:37 -0700

    create goal pipe transform method

--- src/app/goal.pipe.ts
+++ src/app/goal.pipe.ts
@@ -1,12 +1,30 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Project } from './project.model';
 
 @Pipe({
   name: 'goal'
 })
 export class GoalPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(input: Project[], desiredGoal) {
+    var output: Project[] = [];
+    if(desiredGoal === "under2500") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].goal < 2500) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else if (desiredGoal === "under10000") {
+      for (var i = 0; i < input.length; i++) {
+        if (input[i].goal < 10000) {
+          output.push(input[i]);
+        }
+      }
+      return output;
+    } else {
+      return input;
+    }
   }
 
 }
