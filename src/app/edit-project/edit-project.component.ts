c8ee596d454fac38486b26e442ddbcca2de483f2
Brittany Kerr and Nicole Freed <nicole@>
2017-08-24 09:19:30 -0700

    create beginDeletingProject() method

--- src/app/edit-project/edit-project.component.ts
+++ src/app/edit-project/edit-project.component.ts
@@ -19,4 +19,10 @@ export class EditProjectComponent implements OnInit {
     this.projectService.updateProject(projectToUpdate);
   }
 
+  beginDeletingProject(projectToDelete){
+    if(confirm("Are you sure you want to delete this project from the catalog?")){
+      this.projectService.deleteProject(projectToDelete);
+    }
+  }
+
 }
